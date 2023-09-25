957860a79e4b46662119ff796dd7e05d39464c5f
heidi1105 <heidi1105@gmail.com>
2023-09-25 15:18:17 -0600

    Java Fundamentals Part 2

--- Java1Fundamentals/VoiceMessageTest.java
+++ Java1Fundamentals/VoiceMessageTest.java
@@ -1,6 +1,63 @@
+import java.util.ArrayList;
 class VoiceMessageTest{
     public static void main(String[] args){
         VoiceMessageUtil newBot = new VoiceMessageUtil();
         System.out.println(newBot.greetMessage());
+        System.out.println(newBot.greetMessage("Pepper"));
+        System.out.println(newBot.greetMessage(6));
+
+        /* Behind the scene
+        1. When VoiceMessageTest.java is compiled, it reads the instantiation of VoiceMessageUtil class
+        2. The compiler search for VoiceMessageUtil.java in the same package/folder to compile
+        3. Util file is compiled and all the methods in util can be used
+        */
+
+
+        // 5. Casting (implicit & explicit, Null VALUE)
+        int happiness = 10;
+        double doubleHappiness = happiness;
+        // implicit casting: int --> double (no loss of data)
+        System.out.println("Happiness: " + happiness);
+        System.out.println("Happiness (in double): " + doubleHappiness);
+
+        // explicit casting: double --> int (loss of data)
+        double assignmentTime = 1.5;
+        int intAssignmentTime = (int) assignmentTime;
+        System.out.println("assignmentTime: " + assignmentTime);
+        System.out.println("assignmentTime (in int): " + intAssignmentTime);
+
+        // 7. Fixed Array & Loops
+        int[] messagesPerDay = new int[3]; // int cannot be null
+        messagesPerDay[0] = 5;
+        messagesPerDay[1] = 8;
+        messagesPerDay[2] = 6;
+
+        int[] messagesPerDay2 = { 5, 8, 6};
+        String[] messages = {"Please call back!", 
+            "Make sure you install jdk", 
+            "DO NOT INSTALL VS CODE EXTENSION!"};
+        newBot.printArr(messagesPerDay);
+        newBot.printArr(messagesPerDay2);
+        newBot.printArr(messages);
+
+        System.out.println(messages);
+
+        // 8. ArrayList
+        ArrayList<String> reminders = new ArrayList<String>();
+        // push
+        reminders.add("DO NOT INSTALL VS CODE EXTENSION");
+        reminders.add("Do the assignments");
+        reminders.add("Stay hydrated");
+        // pop
+        reminders.remove(1);
+        reminders.remove("Stay hydrated");
+        // update
+        reminders.set(0, "20 minutes rule");
+        // length
+
+        System.out.println(reminders);
+        System.out.println("Length : " + reminders.size());
+
+
     }
 }
--- Java1Fundamentals/VoiceMessageUtil.java
+++ Java1Fundamentals/VoiceMessageUtil.java
@@ -1,5 +1,32 @@
+import java.util.Date;
+
 class VoiceMessageUtil{
+    // specify the return type & parameters
     public String greetMessage(){
-        return "Hi";
+        Date currentTime = new Date();
+        return "Hi, it is "+currentTime ;
     }
+
+    public String greetMessage(String name){
+        return "Hi, "+ name;
+    }
+
+    public String greetMessage(int count){
+        return "Hi * " + count;
+    }
+
+    public void printArr(int[] arr){
+        System.out.println("----- printArr method -----");
+        for(int i= 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    public void printArr(String[] arr){
+        System.out.println("----- printArr method (String[]) -----");
+        for(String element : arr){
+            System.out.println(element);
+        }
+    }
+
 }
